d27b4284bbf9ca816bb11c7465c0e88d66e8507a
idireneyoucef <youcef_idi@hotmail.com>
2018-09-19 00:44:06 +0200

    fetch dont work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React, { Component } from 'react';
+ 
+import React from 'react'; 
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
 
-class App extends Component {
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = { 
+      'etages': []
+    };
+
+  }
+  
+
+  async componentDidMount() { 
+    this.getItems();
+  }
+ getItems(){
+   fetch('/api/allEtage')
+   .then(results=>results.json())
+   .then(parsedJSON=>console.log(parsedJSON.results)); 
+   
+
+ }
   render() {
+    const {etages, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <h2>JUG List</h2>
+          
+          {etages.map((etage,i) =>
+         <div key={i}>
+              {etage.name}
+         </div>
+          )}
+        </div>
       </div>
     );
   }
