6ae8f147d724e4b380eb74e7c7187b10b6b82954
idireneyoucef <youcef_idi@hotmail.com>
2018-09-22 15:10:23 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,50 +1,63 @@
- 
-import React from 'react'; 
+
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css'; 
+import './App.css';
+import Welcome from './Welcome';
+import TodoList from './Todo/TodoList';
 
-class App extends React.Component {
+class App extends Component {
 
-  constructor(){
-    super();
-    this.state = { 
-      'etages': []
-    };
+  constructor(props) {
+    super(props);
+    this.state = {
+      etages: [], 
+      isLoading: true
+    }; 
 
-  }
+  } 
   
+ 
 
-  async componentDidMount() { 
-    this.getItems();
+  async componentDidMount() {
+    this.setState({isLoading: true});
+    const response = await fetch('/api/allEtage')
+    const body = await response.json();
+    this.setState({ etages: body, isLoading: false });
   }
- getItems(){
-   fetch('/api/allEtage')
-   .then(results=>results.json())
-   .then(parsedJSON=>console.log(parsedJSON.results)); 
    
-
- }
   render() {
     const {etages, isLoading} = this.state;
-
     if (isLoading) {
       return <p>Loading...</p>;
     }
 
     return (
+
       <div className="App">
+         <ul>
+            {etages.map(etage =>
+              <li key={etage.idEtage}>
+                  {etage.nomEtage}
+              </li>
+            )}
+          </ul>
+        <TodoList />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+
         </header>
         <div className="App-intro">
+          <Welcome name="IDIRENE Youcef" />
           <h2>JUG List</h2>
-          
-          {etages.map((etage,i) =>
-         <div key={i}>
-              {etage.name}
-         </div>
+
+          {this.state.etages.map(etage =>
+            <div key={etage.idEtage}>
+              {etage.nomEtage}
+            </div>
           )}
+       
+
         </div>
       </div>
     );
